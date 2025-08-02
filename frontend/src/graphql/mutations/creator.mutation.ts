5ae9d124ed19b7c020c6bddd4e5dea72a4262e1a
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-08-02 18:00:07 +0100

    Added mutations

--- frontend/src/graphql/mutations/creator.mutation.ts
+++ frontend/src/graphql/mutations/creator.mutation.ts
@@ -13,3 +13,33 @@ export const CREATE_CREATOR = gql`
       }
       }
     }`;
+
+export const DELETE_TRANSACTION = gql`
+	mutation DeleteCreator($creatorId: ID!) {
+    deleteCreator(creatorId: $creatorId) {
+      _id
+      name
+      bio
+      image
+      links {
+        type
+        url
+      }
+		}
+	}
+`;
+
+export const UPDATE_TRANSACTION = gql`
+	mutation UpdateCreator($input: UpdateCreatorInput!) {
+		updateCreator(input: $input) {
+			_id
+			name
+			bio
+			image
+			links {
+				type
+				url
+			}
+		}
+	}
+`;
